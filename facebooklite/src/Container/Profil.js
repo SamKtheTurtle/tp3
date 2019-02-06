4777019ad36bb93adcee413cc202d4f4c9c84503
DESKTOP-M2JB4NO\SamK <samkdo771@gmail.com>
2019-02-06 12:42:05 +0100

    ajout counter button bonus mais pas encore suavegarde valeur

--- facebooklite/src/Container/Profil.js
+++ facebooklite/src/Container/Profil.js
@@ -11,9 +11,11 @@ export default class Profil extends Component{
               prenom:  'Sam',
               date:  '26/12/1996',
               image: 'http://al-taiclub.com/images/businessman-iphone-clipart-3.png',
-              post: 'Sushi Forever'
+              post: 'Sushi Forever',
+              like: 0
         };
         this.randomColor = this.randomColor.bind(this);
+        this.likeCounter = this.likeCounter.bind(this);
   }
   randomColor(){
     var letters = '0123456789ABCDEF';
@@ -23,27 +25,42 @@ export default class Profil extends Component{
     }
     this.setState({color: colory});    
     }
+
+  likeCounter(){
+    if(this.state.nom === 'CADDEO'){
+        var counterZero = this.state.like +1;
+        this.setState({like: counterZero});
+      }
+      if(this.state.nom === 'PRAT'){
+        var counterOne = this.state.like +1;
+        this.setState({like: counterOne});
+      }
+      if(this.state.nom === 'EPAULARD'){
+        var counterTwo = this.state.like +1;
+        this.setState({like: counterTwo});
+      }
+  }  
     render(){
         return(
         <div className="Page">
             <div className="buttonNav">
                 <button  
               className="button button1" 
-              onClick={() => this.setState({prenom: 'Sam', nom: 'CADDEO', date: '26/12/1996', image: 'http://al-taiclub.com/images/businessman-iphone-clipart-3.png', post: 'Sushi Forever'})}
+              onClick={() => this.setState({prenom: 'Sam', nom: 'CADDEO', date: '26/12/1996', image: 'http://al-taiclub.com/images/businessman-iphone-clipart-3.png', post: 'Sushi Forever', like: 0})}
               >
               Sam
               </button>
 
               <button  
               className="button button2" 
-              onClick={() => this.setState({prenom: 'Arthur', nom: 'PRAT', date: '06/07/1997', image: 'https://lemonlearning.fr/wp-content/uploads/2017/05/boss.png', post: 'Team Pizza'})}
+              onClick={() => this.setState({prenom: 'Arthur', nom: 'PRAT', date: '06/07/1997', image: 'https://lemonlearning.fr/wp-content/uploads/2017/05/boss.png', post: 'Team Pizza', like: 0})}
               >
              Arthur
               </button>
 
               <button  
               className="button button3" 
-              onClick={() => this.setState({prenom: 'Alannah', nom: 'EPAULARD', date: '17/06/1996', image: 'http://designstemplates.in/atozinandhra/images/icons/login.png', post: 'Veganism is Life'})}
+              onClick={() => this.setState({prenom: 'Alannah', nom: 'EPAULARD', date: '17/06/1996', image: 'http://designstemplates.in/atozinandhra/images/icons/login.png', post: 'Veganism is Life',like: 0})}
               >
               Alannah
               </button>
@@ -62,6 +79,7 @@ export default class Profil extends Component{
 
         <div className="Post" style={{background: this.state.color}} >  
         <p>{this.state.post}</p>
+        <button className="buttonLike" onClick={(this.randomColor, this.likeCounter)}>{this.state.like} Super ! </button> 
         </div>   
         </div>  
         
